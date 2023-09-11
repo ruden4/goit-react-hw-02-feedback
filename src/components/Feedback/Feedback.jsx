fc332ef88a8813cebf1c6cc9098a13200af95c20
ruden4 <ruden4@gmail.com>
2023-09-11 21:51:10 +0300

    ver1.0

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,20 +1,39 @@
 import React from "react";
+
 import css from './Feedback.module.css'
 
 export class Feedback extends React.Component {
+    
     state = {
         good: 0,
         neutral: 0,
         bad: 0
     };
 
-    handleFeedback = e => {
-        const feedValue = e.currentTarget.innerText.toLowerCase();
+    handleGoodFeedback = (e) => {
         this.setState((prevState)=>{
             return {
-                feedValue: prevState.good + 1,
-    }});
-        console.log(feedValue);
+                good: prevState.good + 1,
+    }})};
+    handleNeutralFeedback = (e) => {
+        this.setState((prevState)=>{
+            return {
+                neutral: prevState.neutral + 1,
+    }})};
+    handleBadFeedback = (e) => {
+        this.setState((prevState)=>{
+            return {
+                bad: prevState.bad + 1,
+    }})};
+    countTotalFeedback = () => {
+    return this.state.good + this.state.neutral + this.state.bad;
+    };
+    countPositiveFeedbackPercentage = () => {
+    const totalFeedback = this.countTotalFeedback();
+    if (totalFeedback === 0) {
+        return 0;
+    }
+    return Math.trunc((this.state.good / totalFeedback) * 100);
     };
 
     render() {
@@ -22,16 +41,18 @@ export class Feedback extends React.Component {
             <div className={css.container}>
                 <h2 className={css.feedTitle}>Please leave Feedback</h2>
                 <ul className={css.feedBtnList}>
-                    <li className={css.btnListItem}><button className={css.feedBtn} onClick={this.handleFeedback}>Good</button></li>
-                    <li className={css.btnListItem}><button className={css.feedBtn} onClick={this.handleFeedback}>Neutral</button></li>
-                    <li className={css.btnListItem}><button className={css.feedBtn} onClick={this.handleFeedback}>Bad</button></li>
+                    <li className={css.btnListItem}><button className={css.feedBtn} onClick={this.handleGoodFeedback}>Good</button></li>
+                    <li className={css.btnListItem}><button className={css.feedBtn} onClick={this.handleNeutralFeedback}>Neutral</button></li>
+                    <li className={css.btnListItem}><button className={css.feedBtn} onClick={this.handleBadFeedback}>Bad</button></li>
                 </ul>
                 <h2 className={css.feedTitle}>Statistics</h2>
                 <ul className={css.feedStatsList}>
                     <li className={css.statName}>Good<span className={css.statValue}>{this.state.good}</span></li>
                     <li className={css.statName}>Neutral<span className={css.statValue}>{this.state.neutral}</span></li>
                     <li className={css.statName}>Bad<span className={css.statValue}>{this.state.bad}</span></li>
+                    <li className={css.statName}>Total<span className={css.statValueOther}>{this.countTotalFeedback()}</span></li>
                 </ul>
+                <h2 className={css.feedTitle}>Positive Feedback<span className={css.statValue}>{this.countPositiveFeedbackPercentage()}%</span></h2>
             </div>
         )
     }
